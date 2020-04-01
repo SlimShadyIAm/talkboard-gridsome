62dfa1c0847146613c4473971cb017717348a062
SlimShadyIAm <aamir@farooq.xyz>
2020-04-01 13:59:53 +0200

    Design tweaks

--- src/components/Episode.vue
+++ src/components/Episode.vue
@@ -1,5 +1,5 @@
 <template>
-    <!-- <div class="episode-card column is-3">
+	<!-- <div class="episode-card column is-3">
 		<p class="episode-title">{{ title }}</p>
 		<p class="episode-desc">
 			{{
@@ -9,90 +9,90 @@
 					.join(" ") + "..."
 			}}
 		</p>
-    </div>-->
-    <div class="card card-black column is-4">
-        <div class="card-content">
-            <div class="media">
-                <div class="media-content">
-                    <p class="title is-5 has-text-white">{{ episode.title }}</p>
-                    <p class="subtitle is-6 has-text-white">
-                        Season {{ episode.itunes.season }}, Epsiode
-                        {{ episode.itunes.episode }}
-                    </p>
-                </div>
-                <div class="media-right">
-                    <a :href="episode.link" target="_blank" class="button play-button">
-                        <span class="icon has-text-white">
-                            <i class="fas fa-play"></i>
-                        </span>
-                    </a>
-                </div>
-            </div>
+	</div>-->
+	<div class="card card-black column is-4">
+		<div class="card-content">
+			<div class="media">
+				<div class="media-content">
+					<p class="title is-5 has-text-white">{{ episode.title }}</p>
+					<p class="subtitle is-6 has-text-white">
+						Season {{ episode.itunes.season }}, Epsiode
+						{{ episode.itunes.episode }}
+					</p>
+				</div>
+				<div class="media-right">
+					<a :href="episode.link" target="_blank" class="button play-button">
+						<span class="icon has-text-white">
+							<i class="fas fa-play"></i>
+						</span>
+					</a>
+				</div>
+			</div>
 
-            <div class="content has-text-white">
-                {{ episode.contentSnippet }}
-                <br />
-                <time>{{ timestamp }}</time>
-            </div>
-        </div>
-    </div>
+			<div class="content has-text-white">
+				{{ episode.contentSnippet }}
+				<br />
+				<time>{{ timestamp }}</time>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
 var moment = require("moment");
 export default {
-    name: "Episode",
-    props: ["episode"],
-    data() {
-        return {
-            timestamp: moment(this.episode.pubDate).fromNow()
-        };
-    }
+	name: "Episode",
+	props: ["episode"],
+	data() {
+		return {
+			timestamp: moment(this.episode.pubDate).fromNow()
+		};
+	}
 };
 </script>
 
 <style>
 .card-black {
-    background-color: rgb(31, 32, 33) !important;
-    margin-right: 10px;
-    width: auto;
+	background-color: rgb(31, 32, 33) !important;
+	margin-right: 10px;
+	width: auto;
 }
 
 @media (max-width: 768px) {
-    .card {
-        margin-right: 0px;
-        margin-bottom: 10px;
-    }
+	.card {
+		margin-right: 0px;
+		margin-bottom: 10px;
+	}
 }
 
 .episode-title {
-    color: #fff;
-    font-size: 22px;
-    font-weight: 600;
+	color: #fff;
+	font-size: 22px;
+	font-weight: 600;
 }
 
 .episode-desc {
-    color: #fff;
+	color: #fff;
 }
 
 .play-button {
-    border-radius: 50% !important;
-    background-color: rgb(0, 175, 245) !important;
-    border: none;
+	border-radius: 50% !important;
+	background-color: rgb(0, 175, 245) !important;
+	border: none !important;
 }
 
 .play-button i {
-    margin-left: 2px;
+	margin-left: 2px;
 }
 
 .media {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 100%;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	height: 100%;
 }
 
 time {
-    color: rgb(175, 175, 175);
+	color: rgb(175, 175, 175);
 }
 </style>
--- src/components/WhereToListen.vue
+++ src/components/WhereToListen.vue
@@ -1,35 +1,35 @@
 <template>
-    <section class="hero where-to-listen">
-        <div class="hero-body reset">
-            <div class="container">
-                <div class="columns">
-                    <div class="column is-3">
-                        <!-- <p class="listen-on">Listen on:</!-->
-                        <p class="title has-text-white has-text-centered">Where to listen</p>
-                        <div class="badges container">
-                            <a href="https://open.spotify.com/show/1sCjmMpUDqnJNU54KwRf2Z">
-                                <img class="badge" src="@/assets/spotify-badge.png" />
-                            </a>
-                            <br />
-                            <a href="https://podcasts.apple.com/gb/podcast/talkboard/id1469760640">
-                                <img class="badge" src="@/assets/apple-badge.png" />
-                            </a>
-                            <div class="rss">
-                                <a
-                                    class="button copy-button is-info has-text-white"
-                                    v-clipboard:copy="message"
-                                >Copy RSS Link</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="column">
-                        <p class="title has-text-white has-text-centered-mobile">Latest episodes</p>
-                        <Episodes></Episodes>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+	<section class="hero where-to-listen">
+		<div class="hero-body reset">
+			<div class="container">
+				<div class="columns">
+					<div class="column is-3">
+						<!-- <p class="listen-on">Listen on:</!-->
+						<p class="title has-text-white has-text-centered">Where to listen</p>
+						<div class="badges container">
+							<a href="https://open.spotify.com/show/1sCjmMpUDqnJNU54KwRf2Z">
+								<img class="badge" src="@/assets/spotify-badge.png" />
+							</a>
+							<br />
+							<a href="https://podcasts.apple.com/gb/podcast/talkboard/id1469760640">
+								<img class="badge" src="@/assets/apple-badge.png" />
+							</a>
+							<div class="rss">
+								<a
+									class="button copy-button is-info has-text-white"
+									v-clipboard:copy="message"
+								>Copy RSS Link</a>
+							</div>
+						</div>
+					</div>
+					<div class="column">
+						<p class="title has-text-white has-text-centered-mobile">Latest episodes</p>
+						<Episodes></Episodes>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
 </template>
 
 <script>
@@ -40,59 +40,58 @@ VueClipboard.config.autoSetContainer = true;
 Vue.use(VueClipboard);
 
 export default {
-    name: "WhereToListen",
-    components: {
-        Episodes
-    },
-    data() {
-        return {
-            message: "https://rss.whooshkaa.com/rss/podcast/id/7841"
-        };
-    }
+	name: "WhereToListen",
+	components: {
+		Episodes
+	},
+	data() {
+		return {
+			message: "https://rss.whooshkaa.com/rss/podcast/id/7841"
+		};
+	}
 };
 </script>
 
 <style>
 .where-to-listen {
-    background-color: #272829;
+	background-color: #272829;
 }
 
 .reset {
-    padding-top: 30px !important;
-    padding-bottom: 50px !important;
+	padding-top: 30px !important;
+	padding-bottom: 50px !important;
 }
 
 .badges {
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-    height: 80%;
-    width: 100%;
+	display: flex;
+	justify-content: center;
+	flex-direction: column;
+	height: 80%;
+	width: 100%;
 }
 
 .badge {
-    width: 225px;
-    margin: 10px;
-    display: block;
-    margin: 0 auto;
+	width: 225px;
+	margin: 10px;
+	display: block;
+	margin: 0 auto;
 }
 
 .rss {
-    margin-top: 30px;
-    display: inline-block;
+	margin-top: 30px;
+	display: flex;
+	justify-content: center;
 }
 
 .copy-button {
-    display: block;
-    margin: 0 auto;
-    width: 100%;
-    max-width: 225px;
-    border: none;
+	width: 100%;
+	max-width: 225px;
+	border: none;
 }
 
 .listen-on {
-    color: #fff;
-    font-size: 2.5em;
-    margin-right: 30px;
+	color: #fff;
+	font-size: 2.5em;
+	margin-right: 30px;
 }
 </style>
