2399bb03ba5f80d87c875127fa02f95592f61e8b
SlimShadyIAm <aamir@farooq.xyz>
2020-04-01 13:57:14 +0200

    Import Patreon API data

--- gridsome.server.js
+++ gridsome.server.js
@@ -4,13 +4,38 @@
 
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
+const axios = require("axios");
+module.exports = function(api) {
+	api.loadSource(async actions => {
+		const { data } = await axios.get(
+			"https://www.patreon.com/api/oauth2/api/campaigns/3196722/pledges",
+			{
+				headers: {
+					authorization: `Bearer ${process.env.PATREON_TOKEN}`
+				}
+			}
+		);
 
-module.exports = function (api) {
-  api.loadSource(({ addCollection }) => {
-    // Use the Data Store API here: https://gridsome.org/docs/data-store-api/
-  })
+		const collection = actions.addCollection({
+			typeName: "Patrons"
+		});
+		// console.log(data);
+		for (const pledge of data.data) {
+			if (pledge.type == "pledge") {
+				if (pledge.attributes.amount_cents >= 1000) {
+					const id = pledge.relationships.patron.data.id;
 
-  api.createPages(({ createPage }) => {
-    // Use the Pages API here: https://gridsome.org/docs/pages-api/
-  })
-}
+					var name = data.included.filter(userObject => {
+						return userObject.id == id;
+					});
+
+					collection.addNode({
+						id: id,
+						name: name[0].attributes.vanity,
+						fullName: name[0].attributes.full_name
+					});
+				}
+			}
+		}
+	});
+};
