e8228657a62b727135b85aa0cf68f68f3383672a
Aamir Farooq <aamir@farooq.xyz>
2020-09-10 21:04:29 +0200

    Fixes

--- gridsome.server.js
+++ gridsome.server.js
@@ -6,36 +6,46 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 const axios = require("axios");
 module.exports = function(api) {
-	api.loadSource(async actions => {
-		const { data } = await axios.get(
-			"https://www.patreon.com/api/oauth2/api/campaigns/3196722/pledges",
-			{
-				headers: {
-					authorization: `Bearer ${process.env.PATREON_TOKEN}`
-				}
-			}
-		);
+  api.loadSource(async (actions) => {
+    const { data } = await axios.get(
+      "https://www.patreon.com/api/oauth2/api/campaigns/3196722/pledges",
+      {
+        headers: {
+          authorization: `Bearer ${process.env.PATREON_TOKEN}`,
+        },
+      }
+    );
 
-		const collection = actions.addCollection({
-			typeName: "Patrons"
-		});
-		// console.log(data);
-		for (const pledge of data.data) {
-			if (pledge.type == "pledge") {
-				if (pledge.attributes.amount_cents >= 1000) {
-					const id = pledge.relationships.patron.data.id;
+    const collection = actions.addCollection({
+      typeName: "Patrons",
+    });
+    // console.log(data);
 
-					var name = data.included.filter(userObject => {
-						return userObject.id == id;
-					});
+    pledges = [];
 
-					collection.addNode({
-						id: id,
-						name: name[0].attributes.vanity,
-						fullName: name[0].attributes.full_name
-					});
-				}
-			}
-		}
-	});
+    for (const pledge of data.data) {
+      if (pledge.type == "pledge") {
+        pledges.push(pledge);
+        if (pledge.attributes.amount_cents >= 1000) {
+          const id = pledge.relationships.patron.data.id;
+
+          var name = data.included.filter((userObject) => {
+            return userObject.id == id;
+          });
+          collection.addNode({
+            id: id,
+            name: name[0].attributes.vanity,
+            fullName: name[0].attributes.full_name,
+          });
+        }
+      }
+    }
+    if (data.data.length > 0) {
+      collection.addNode({
+        id: -1,
+        name: "No current Patrons",
+        fullName: "No currnet Patrons",
+      });
+    }
+  });
 };
--- src/components/Patrons.vue
+++ src/components/Patrons.vue
@@ -1,33 +1,37 @@
 <template>
-	<div class="patrons-background">
-		<h3 class="title has-text-centered has-text-white">
-			A big thank you to our top tier Patrons, their contributions help
-			the show operate!
-		</h3>
-		<div class="patrons-names container has-text-centered">
-			<span
-				class="patron-name is-italic"
-				v-for="(patron, index) in $static.allPatrons.edges"
-				:key="patron.id"
-				>{{ patron.node.name ? patron.node.name : patron.node.fullName
-				}}<span class="bullet">{{
-					index != Object.keys($static.allPatrons.edges).length - 1
-						? "&bull;"
-						: ""
-				}}</span></span
-			>
-		</div>
-		<!-- <h3 class="subtitle has-text-centered has-text-white">
+  <div class="patrons-background">
+    <h3 class="title has-text-centered has-text-white">
+      A big thank you to our top tier Patrons, their contributions help
+      the show operate!
+    </h3>
+    <div class="patrons-names container has-text-centered">
+      <span
+        class="patron-name is-italic"
+        v-for="(patron, index) in $static.allPatrons.edges"
+        :key="patron.id"
+      >
+        {{ patron.node.name ? patron.node.name : patron.node.fullName
+        }}
+        <span class="bullet">
+          {{
+          index != Object.keys($static.allPatrons.edges).length - 1
+          ? "&bull;"
+          : ""
+          }}
+        </span>
+      </span>
+    </div>
+    <!-- <h3 class="subtitle has-text-centered has-text-white">
 			Would you like to contribute to the show as well? Check out our
 			Patreon!
-		</h3> -->
-		<a
-			href="https://www.patreon.com/bePatron?u=24427643"
-			class="button patreon-button"
-			><span class="icon is-small"><i class="fab fa-patreon"></i></span
-			><span>Support us on Patreon!</span></a
-		>
-	</div>
+    </h3>-->
+    <a href="https://www.patreon.com/bePatron?u=24427643" class="button patreon-button">
+      <span class="icon is-small">
+        <i class="fab fa-patreon"></i>
+      </span>
+      <span>Support us on Patreon!</span>
+    </a>
+  </div>
 </template>
 
 <static-query>
@@ -46,53 +50,53 @@ query {
 
 <script>
 export default {
-	name: "Patrons"
+  name: "Patrons",
 };
 </script>
 
 <style scoped>
 .title {
-	font-size: 22px;
-	margin-bottom: 10px !important;
+  font-size: 22px;
+  margin-bottom: 10px !important;
 }
 
 .patrons-background {
-	background-color: rgb(31, 31, 31);
-	padding: 50px;
-	display: flex;
-	align-items: center;
-	flex-direction: column;
+  background-color: rgb(31, 31, 31);
+  padding: 50px;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
 }
 .patrons-names {
-	width: 100%;
-	text-align: center;
-	margin-top: 30px;
-	margin-bottom: 30px;
+  width: 100%;
+  text-align: center;
+  margin-top: 30px;
+  margin-bottom: 30px;
 }
 
 .patron-name {
-	color: #fff;
-	font-size: 18px;
+  color: #fff;
+  font-size: 18px;
 }
 
 .bullet {
-	margin-left: 10px;
-	margin-right: 10px;
+  margin-left: 10px;
+  margin-right: 10px;
 }
 .button {
-	margin: 5px;
-	border: none;
-	color: #fff;
-	width: 225px;
+  margin: 5px;
+  border: none;
+  color: #fff;
+  width: 225px;
 }
 
 .patreon-button {
-	background-color: #f96854;
+  background-color: #f96854;
 }
 
 @media (max-width: 768px) {
-	.button {
-		width: 100%;
-	}
+  .button {
+    width: 100%;
+  }
 }
 </style>
